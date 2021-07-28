43168765ee92f3db3d44675ec2e21ea6908c7de6
randomdrag <sumitkumar9006@gmail.com>
2021-07-28 20:15:29 +0530

    starting file

---
+++ src/page/Home.js
@@ -0,0 +1,19 @@
+
+import React from 'react';
+
+
+class Home extends React.Component {
+
+
+    render() {
+
+        return (
+            <div>hello from home </div>
+        );
+    }
+
+
+
+}
+
+export default Home;
---
+++ src/routes/App.js
@@ -0,0 +1,26 @@
+import React from 'react';
+
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+
+
+import Home from '../page/Home';
+
+function App() {
+
+  return (
+
+    <Router>
+
+      <Switch>
+
+        <Route path="/" exact component={Home} />
+      </Switch>
+
+
+
+    </Router>
+
+  );
+}
+
+export default App;
